28adf3ab5f6cdfd380b5cd99a6944fb6b080147c
TiagoUniverse <tiagocesar68@gmail.com>
2023-12-11 14:37:25 -0300

    Criação de opções do menu dentro de um loop de repetição e com o tratamento de erros

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import entities.Cds_controller;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -8,7 +9,22 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Cds_controller cd_controller = new Cds_controller();
 
-        cd_controller.menu();
+        int escolha;
+        do{
+
+            try{
+                escolha = cd_controller.menu(scanner);
+            } catch (InputMismatchException e){
+                System.out.println("Tipo de escolha incorreto. Por favor, escolha um valor numérico (Ex: 1) \n");
+                // Um tipo inválido para resetar o menu
+                escolha = 99;
+                scanner.nextLine();
+            }
+
+            } while (escolha != 0);
+           
+
+
 
         scanner.close();
     }
--- src/entities/Cds_controller.java
+++ src/entities/Cds_controller.java
@@ -1,10 +1,24 @@
 package entities;
 
+import java.util.Scanner;
+
 public class Cds_controller {
 
-    public void menu(){
+    public int menu(Scanner scanner){
         System.out.println("====== Loja de CDS =======");
         System.out.println("Seja bem vindo à loja de CDS. Selecione uma opção abaixo e boa compra! \n");
+        System.out.println("================================");
+        System.out.println("Selecione uma opção: \n");
+        System.out.println("1- Exibir o estoque ");
+        System.out.println("2- Adicionar um CD");
+        System.out.println("3- Pesquisar um CD");
+        System.out.println("4- Comprar um CD");
+        System.out.println("5- Remover um CD");
+        System.out.println("0 - Encerrar o programa");
+
+        int escolha = scanner.nextInt();
+
+        return escolha;
     }
 
 }
