e3cc4e11800d39d6d2b04de04e6e5f75d55f076f
TiagoUniverse <tiagocesar68@gmail.com>
2023-12-13 15:02:39 -0300

    Adicionei validação no preço do cd

--- src/entities/Cd_Factory.java
+++ src/entities/Cd_Factory.java
@@ -1,16 +1,32 @@
 package entities;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Cd_Factory {
 
-    public static Cds adicionar_cd(Scanner scanner){
+    public static Cds adicionar_cd(Scanner scanner) {
 
         System.out.print("Insira o nome do cd: ");
-        String nome = scanner.nextLine(); //cd
+        String nome = scanner.nextLine();
 
-        System.out.print("Insira o preço do cd: ");
-        double valor = scanner.nextDouble();
+
+        double valor;
+        int validacao = 0;
+        do {
+            try {
+                System.out.print("Insira o preço do cd: ");
+                valor = scanner.nextDouble();
+                validacao = 1;
+
+            } catch (InputMismatchException e) {
+                System.out.println("\n Preço do produto inválido. Por favor, insira um valor inteiro ou decimal (Ex: 35.99)");
+                scanner.nextLine();
+                valor = 0;
+                validacao = 0;
+            }
+        } while (validacao == 0);
+
 
         scanner.nextLine();
         System.out.println("Insira o nome do artista: ");
@@ -18,8 +34,7 @@ public class Cd_Factory {
         scanner.nextLine();
 
 
-
-        return new Cds( nome, valor, nome_artista);
+        return new Cds(nome, valor, nome_artista);
 
     }
 }
